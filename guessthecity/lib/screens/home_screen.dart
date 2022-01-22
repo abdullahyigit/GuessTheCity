92b6acc2c0c203a5172f96cdac125ae019986c22
abdullahyigit <abdullahmygt@gmail.com>
2022-01-23 02:01:28 +0300

    added home screen

---
+++ guessthecity/lib/screens/home_screen.dart
@@ -0,0 +1,216 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Home_Screen extends StatefulWidget {
+  const Home_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Home_Screen> createState() => _Home_ScreenState();
+}
+
+class _Home_ScreenState extends State<Home_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GUESS THE CITY',
+            style:
+                TextStyle(color: Colors.black, fontFamily: 'YOUR_FONT_FAMILY')),
+        centerTitle: true,
+        backgroundColor: Colors.black45,
+      ),
+      body: Container(
+        color: const Color(0xfff5f5dc),
+        child: Column(
+          children: <Widget>[
+//===========================AVRUPA=============================================
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd5b791),
+                  border: Border.all(
+                    color: const Color(0xffd5b791),
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/europe3.png"),
+                      fit: BoxFit.fitHeight,
+                      alignment: Alignment.centerRight),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 12),
+                    const Text(
+                      'Avrupa',
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    const Text(
+                      '(0/80)',
+                      style: TextStyle(fontSize: 27),
+                    )
+                  ],
+                ),
+              ),
+            ),
+//===========================ASYA=============================================
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd5b791),
+                  border: Border.all(
+                    color: const Color(0xffd5b791),
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/asia.jpg"),
+                      fit: BoxFit.fitHeight,
+                      alignment: Alignment.centerRight),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 12),
+                    const Text(
+                      'Asya',
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    const Text(
+                      '(0/80)',
+                      style: TextStyle(fontSize: 27),
+                    )
+                  ],
+                ),
+              ),
+            ),
+//===========================KUZEYAMERİKA=============================================
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd5b791),
+                  border: Border.all(
+                    color: const Color(0xffd5b791),
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/europe3.png"),
+                      fit: BoxFit.fitHeight,
+                      alignment: Alignment.centerRight),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 12),
+                    const Text(
+                      'Kuzey Amerika',
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    const Text(
+                      '(0/80)',
+                      style: TextStyle(fontSize: 27),
+                    )
+                  ],
+                ),
+              ),
+            ),
+//===========================GÜNEYAMERİKA=============================================
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd5b791),
+                  border: Border.all(
+                    color: const Color(0xffd5b791),
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/europe3.png"),
+                      fit: BoxFit.fitHeight,
+                      alignment: Alignment.centerRight),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 12),
+                    const Text(
+                      'Güney Amerika',
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    const Text(
+                      '(0/80)',
+                      style: TextStyle(fontSize: 27),
+                    )
+                  ],
+                ),
+              ),
+            ),
+//===========================AVUSTRALYA=============================================
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd5b791),
+                  border: Border.all(
+                    color: const Color(0xffd5b791),
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/europe3.png"),
+                      fit: BoxFit.fitHeight,
+                      alignment: Alignment.centerRight),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 12),
+                    const Text(
+                      'Afrika',
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    const Text(
+                      '(0/80)',
+                      style: TextStyle(fontSize: 27),
+                    )
+                  ],
+                ),
+              ),
+            ),
+//===========================AVUSTRALYA=============================================
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffd5b791),
+                  border: Border.all(
+                    color: const Color(0xffd5b791),
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/europe3.png"),
+                      fit: BoxFit.fitHeight,
+                      alignment: Alignment.centerRight),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 12),
+                    const Text(
+                      'Avustralya',
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    const Text(
+                      '(0/80)',
+                      style: TextStyle(fontSize: 27),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
